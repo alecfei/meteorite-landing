7dfb410432d0dc1d0998388c6e722aa0dec807bc
hduser@alecfei-linuxlaptop <2022173@student.cct.ie>
2024-06-13 16:00:06 +0100

    EDA

--- Meteorite info EDA.sql
+++ Meteorite info EDA.sql
@@ -3,4 +3,60 @@ GO
 
 SELECT * FROM MeteoriteInfo;
 
+/*
+SELECT recclass
+    -- , nametype
+    -- , fall
+    , COUNT(recclass) AS [class numbers]
+    -- , COUNT(nametype) AS [type counts]
+    -- , COUNT(fall) AS [whether landing or not]
+FROM MeteoriteInfo
+GROUP BY recclass 
+    -- , nametype 
+    -- , fall
+ORDER BY recclass
+--     , [class numbers] DESC
+--     , [type counts] DESC
+--     , [whether landing or not]
+;
+*/
+
+
 -- Count of recclass, nametype and fall
+SELECT recclass
+    , COUNT(recclass) AS [class numbers]
+FROM MeteoriteInfo
+GROUP BY recclass 
+ORDER BY [class numbers] DESC
+;
+
+SELECT nametype
+    , COUNT(nametype) AS [type counts]
+FROM MeteoriteInfo
+GROUP BY nametype 
+--ORDER BY [type counts]
+;
+
+SELECT fall
+    , COUNT(fall) AS [whether landing or not]
+FROM MeteoriteInfo
+GROUP BY fall
+--ORDER BY [whether landing or not]
+;
+
+
+-- the numbers of different meteorite classes and their nametypes that fell or not
+SELECT recclass
+    , nametype
+    , fall
+    , COUNT(*) AS [class numbers]
+FROM MeteoriteInfo
+GROUP BY recclass
+    , nametype
+    , fall
+ORDER BY recclass
+    --, [class numbers] DESC  
+    --, nametype
+    --, fall
+;
+
--- Meteorite mass EDA.sql
+++ Meteorite mass EDA.sql
@@ -1,12 +1,47 @@
 USE Meteorite;
 GO
 
--- check averagemass of each class
+-- aggregate mass of each class
+SELECT recclass
+    , AVG([mass (g)]) AS [average mass]
+    , MAX([mass (g)]) AS [maximum mass]
+    , MIN([mass (g)]) AS [minimum mass]
+FROM MeteoriteMass
+GROUP BY recclass
+ORDER BY 
+   -- recclass
+    [average mass] DESC
+;
+
+
+/*
 --CREATE VIEW AverageMeteoriteMass AS
-SELECT id, name, recclass, [mass (g)], AVG([mass (g)]) OVER (PARTITION BY recclass) as averagemass
+SELECT id
+    , name
+    , recclass
+    , [mass (g)]
+    , AVG([mass (g)]) OVER (PARTITION BY recclass) AS averagemass
 FROM MeteoriteMass
 ;
 
-SELECT * FROM AverageMeteoriteMass
-ORDER BY recclass, averagemass
+SELECT recclass
+    , AVG([mass (g)]) AS [average mass]
+FROM MeteoriteMass
+GROUP BY recclass
+ORDER BY [average mass] DESC
 ;
+
+SELECT recclass 
+    , MAX([mass (g)]) AS [maximum mass]
+FROM MeteoriteMass
+GROUP BY recclass
+ORDER BY [maximum mass] DESC
+;
+
+SELECT recclass
+    , MIN([mass (g)]) AS [minimum mass]
+FROM MeteoriteMass
+GROUP BY recclass
+ORDER BY [minimum mass]
+;
+*/
